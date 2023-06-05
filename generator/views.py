e02f0ef86b318cc8e6aecefb767ea98e0a4c508f
Alex <4422504a@gmail.com>
2023-06-05 21:07:11 +0300

    '3'

--- generator/views.py
+++ generator/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
+    context = {
+        'title': 'Создайте свой пароль',
 
-    return render(request, 'generator/index.html')
+    }
+
+    return render(request, 'generator/index.html', context=context)

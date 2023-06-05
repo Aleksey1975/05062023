b7492bddf46580e86e5b6dbdb2ad1339ed1ae46e
Alex <4422504a@gmail.com>
2023-06-05 22:05:26 +0300

    '5'

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import string
+import random
+
 
 def index(request):
     context = {
@@ -10,8 +13,23 @@ def index(request):
     return render(request, 'generator/index.html', context=context)
 
 def password(request):
+    nabor = string.ascii_lowercase
+    if request.GET.get('uppercase'):
+        nabor += string.ascii_uppercase
+    if request.GET.get('digit'):
+        nabor += '0123456789'
+    if request.GET.get('special'):
+        nabor += string.punctuation
+
+    L = int(request.GET.get('length'))
+    parol = ''
+    nabor = list(nabor)
+    print(nabor)
+    for _ in range(L):
+        parol += random.choice(nabor)
     context = {
         'title': 'Ваш пароль',
+        'parol': parol
 
     }
 
